c409fc85ce16282aa58153d839f5c6b73189887c
Lucas Dias <lucasdias.ld10@gmail.com>
2023-03-03 05:15:48 -0300

    correção no ranking e url

--- src/controller/Ranking.js
+++ src/controller/Ranking.js
@@ -5,9 +5,8 @@ export async function getRanking(req, res) {
 
     try{
         const users = await db.query(`SELECT id, name, links_count, views_count FROM users ORDER BY views_count DESC LIMIT 10`)
-        console.log(users.rows)
 
-        res.send('Ok!')
+        res.send(users.rows)
     }
     catch (error) {
         res.status(500).send(error.message)
