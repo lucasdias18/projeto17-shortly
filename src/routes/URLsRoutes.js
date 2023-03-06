394fde331717891f05d2207165a46536328e0606
Lucas Dias <lucasdias.ld10@gmail.com>
2023-03-06 17:19:56 -0300

    pequenos ajustes para o deploy e comentários apagados

--- src/routes/URLsRoutes.js
+++ src/routes/URLsRoutes.js
@@ -1,9 +1,8 @@
 import { Router } from 'express'
-// import { getGames, postGame } from '../controller/Game.js'
 import { deleteURL, getUrlId, openUrl, postURL } from '../controller/URLs.js'
 import { validateSchema } from '../middleware/validateSchema.js'
 import { urlSchema } from '../schema/urlSchema.js'
-// import { gameSchema } from '../schema/GameSchema.js'
+
 
 const urlRouter = Router()
 
@@ -12,6 +11,5 @@ urlRouter.get("/urls/:id", getUrlId)
 urlRouter.get("/urls/open/:shortUrl", openUrl)
 urlRouter.delete("/urls/:id", deleteURL)
 
-// , validateSchema(urlSchema)
 
 export default urlRouter
